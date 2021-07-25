25d220984372101db0ce3ea30cd8aaf4adec1922
dpmerino <diegopaul7@outlook.com>
2021-07-25 00:15:21 -0500

    Firebase

--- src/app/service/vacuna.service.ts
+++ src/app/service/vacuna.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Vacuna } from '../models/vacuna';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VacunaService {
+
+  vacunasCollection!: AngularFirestoreCollection<Vacuna>;
+  vacunas!: Observable<Vacuna[]>;
+  vacunasDoc!: AngularFirestoreDocument<Vacuna>;
+  
 
   constructor() { }
   getAllVacuna(){
